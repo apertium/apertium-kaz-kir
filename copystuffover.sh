89e23995e01c780d94c4038fcbb5fea637344f4e
Jonathan North Washington <jonathan.n.washington@gmail.com>
2013-04-24 20:55:26 +0000

    no longer needed

--- copystuffover.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp ../apertium-kir/apertium-kir.kir.lexc ./apertium-kaz-kir.kir.lexc
-cp ../apertium-kir/apertium-kir.kir.twol ./apertium-kaz-kir.kir.twol
-cp ../apertium-kaz/apertium-kaz.kaz.lexc ./apertium-kaz-kir.kaz.lexc
-cp ../apertium-kaz/apertium-kaz.kaz.twol ./apertium-kaz-kir.kaz.twol
