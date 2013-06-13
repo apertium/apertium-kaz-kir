0a9b838fa099a012c3a035d562ee4c89786363ce
Jonathan North Washington <jonathan.n.washington@gmail.com>
2013-06-13 19:54:05 +0000

    maybe now?

--- dev/testvoc/inconsistency.sh
+++ dev/testvoc/inconsistency.sh
@@ -9,7 +9,7 @@ hfst-fst2strings ../../kir-kaz.automorf.hfst | grep -v 'REGEX' | grep -v ':<:' |
 	lt-proc -b ../../kir-kaz.autobil.bin |  tee $TMPDIR/$DIR.tmp_testvoc2.txt |
         apertium-transfer -b ../../apertium-kaz-kir.kir-kaz.t1x  ../../kir-kaz.t1x.bin | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
         hfst-proc -d ../../kir-kaz.autogen.hfst > $TMPDIR/$DIR.tmp_testvoc4.txt
-paste -d ¶ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt | sed 's/\^.<sent>\$//g' | sed 's/¶/   --------->  /g'
+paste -d \t $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt | sed 's/\^.<sent>\$//g' | sed 's/\t/   --------->  /g'
 
 elif [[ $DIR = "kaz-kir" ]]; then
 
@@ -18,7 +18,7 @@ hfst-fst2strings ../../kaz-kir.automorf.hfst | grep -v 'REGEX' | grep -v ':<:' |
 	lt-proc -b ../../kaz-kir.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |
         apertium-transfer -b ../../apertium-kaz-kir.kaz-kir.t1x  ../../kaz-kir.t1x.bin | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
         hfst-proc -d ../../kaz-kir.autogen.hfst > $TMPDIR/$DIR.tmp_testvoc4.txt
-paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt| sed 's/\^.<sent>\$//g' | sed 's/¶/   --------->  /g'
+paste -d \t $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt| sed 's/\^.<sent>\$//g' | sed 's/\t/   --------->  /g'
 
 
 else
