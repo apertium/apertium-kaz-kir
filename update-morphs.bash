d124f9c42b6655dc877ba8c3a8c4cb5ffa7bd9fc
Jonathan North Washington <jonathan.n.washington@gmail.com>
2013-08-27 05:48:33 +0000

    updated update-morphs script

--- update-morphs.bash
+++ update-morphs.bash
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-#assuming that you have the whole apertium tree in your source dir. and you are in kaz-kir directory
+##assuming that you have the whole apertium tree in your source dir. and you are in kaz-kir directory
+#
+#python3 ../../trunk/apertium-tools/trim-lexc.py apertium-kaz-kir.kaz-kir.dix ../apertium-kaz/apertium-kaz.kaz.lexc ../apertium-kir/apertium-kir.kir.lexc
+#
+#cp /tmp/apertium-kaz.kaz.lexc.trimmed apertium-kaz-kir.kaz.lexc
+#cp /tmp/apertium-kir.kir.lexc.trimmed apertium-kaz-kir.kir.lexc
+#
+#DIFF=$(diff ../apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-kir.kaz.twol)
+#if [ "$DIFF" != "" ]; then
+#	cp ../apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-kir.kaz.twol
+#fi;
+#cp ../apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-kir.kaz-kir.rlx
+cp ../../languages/apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-kir.kaz-kir.rlx
 
-python3 ../../trunk/apertium-tools/trim-lexc.py apertium-kaz-kir.kaz-kir.dix ../apertium-kaz/apertium-kaz.kaz.lexc ../apertium-kir/apertium-kir.kir.lexc
-
-cp /tmp/apertium-kaz.kaz.lexc.trimmed apertium-kaz-kir.kaz.lexc
-cp /tmp/apertium-kir.kir.lexc.trimmed apertium-kaz-kir.kir.lexc
-
-DIFF=$(diff ../apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-kir.kaz.twol)
-if [ "$DIFF" != "" ]; then
-	cp ../apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-kir.kaz.twol
-fi;
-cp ../apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-kir.kaz-kir.rlx
-
-DIFF=$(diff ../apertium-kir/apertium-kir.kir.twol apertium-kaz-kir.kir.twol)
-if [ "$DIFF" != "" ]; then
-	cp ../apertium-kir/apertium-kir.kir.twol apertium-kaz-kir.kir.twol
-fi;
-cp ../apertium-kir/apertium-kir.kir.rlx apertium-kaz-kir.kir-kaz.rlx
+#DIFF=$(diff ../apertium-kir/apertium-kir.kir.twol apertium-kaz-kir.kir.twol)
+#if [ "$DIFF" != "" ]; then
+#	cp ../apertium-kir/apertium-kir.kir.twol apertium-kaz-kir.kir.twol
+#fi;
+#cp ../apertium-kir/apertium-kir.kir.rlx apertium-kaz-kir.kir-kaz.rlx
+cp ../../languages/apertium-kir/apertium-kir.kir.rlx apertium-kaz-kir.kir-kaz.rlx
 
 exit 0
 
