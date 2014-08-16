f259bdea8785f7939a8d034d7ce691fa562b15dc
Francis M. Tyers <ftyers@prompsit.com>
2014-08-16 20:50:15 +0000

    remove old sh files

--- texts/diff.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wdiff $1.kaz-kir.txt $1.kaz-kir-postedited.txt | colordiff
--- texts/story.eval.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-../../trunk/apertium-eval-translator/apertium-eval-translator-line.pl -t texts/story.kaz-kir.2.txt -r texts/story.kaz-kir-postedited.txt
